e753c5b7112c13da6ee209ece44529f77c4c1538
Aoife <imatt.lino@gmail.com>
2025-03-16 15:26:32 -0300

    adicionando e testando responsividade quanto à presença de nomes na lista e limpeza

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ function addAmigos() {
     amigos.push(amigoInput);
     console.log(amigoInput);
     exibirTextoNaTela(".section-title", "Amigo adicionado! Insira o nome de outro amigo.");
-    
+
   // Verificando se o número mínimo de amigos para o sorteio foi atingido
   // Só exibe a mensagem de quantidade mínima uma vez, quando o número de amigos atingir o mínimo
   if (amigos.length >= numeroMinimoDeParticipantes && !mensagemMinimaExibida) {
@@ -51,16 +51,66 @@ function addAmigos() {
   exibirListaDeAmigos();
 }
 
+// Atualizando a lista e exibindo na tela
 function exibirListaDeAmigos() {
+    //html onde a lista será exibida
+
     let listaAmigos = document.querySelector("#listaAmigos");
+
+    // Limpando a lista após adicionar um nome
     listaAmigos.innerHTML = "";
+    // Loop para percorrer o array e a criação de um <li> para cada valor nome
   
     for (let i = 0; i < amigos.length; i++) {
       let li = document.createElement("li");
-      li.textContent = amigos[i];
-      listaAmigos.appendChild(li);
+      li.textContent = amigos[i]; // Define o valor do conteúdo da li com base no valor da array amigos
+      listaAmigos.appendChild(li); // adicionando li na lista
     }
   }
   
+  function sortearAmigo() {
+    // Verificando se há nomes na lista
 
+    if (amigos.length === 0) {
+        exibirTextoNaTela(".section-title", "Não há nomes para sortear.");
+        return;
+      }
+      if (amigos.length < 3) {
+        exibirTextoNaTela(
+          ".section-title",
+          "Insira no mínimo 3 nomes para sortear."
+        );
+        return;
+      }
+    const randomIndex = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[randomIndex];
+    exibirTextoNaTela("#resultado", `O amigo sorteado foi: ${amigoSorteado}!`);
+    exibirTextoNaTela(".section-title", `Parabéns ${amigoSorteado}!`);
+    amigos = [];
+    limparListaNomes();
+    document.querySelector("#resetar").removeAttribute("disabled");
+  }
+  
+  function limparListaNomes() {
+    document.querySelector("#listaAmigos").innerHTML = "";
+  }
+  
+  function reiniciarSorteio() {
+    amigos = [];
+    limparListaNomes();
+    exibirTextoNaTela(".main-title", "Amigo Secreto");
+    exibirTextoNaTela(".section-title", "Digite o nome dos seus amigos");
+    exibirTextoNaTela("#resultado", "");
+    document.querySelector("#resetar").setAttribute("disabled", true);
+    mensagemMinimaExibida = false;
+  }
+  
+  document.querySelector("#resetar").addEventListener("click", reiniciarSorteio);
+  
+  document.querySelector("#amigo").addEventListener("keydown", function (event) {
+    if (event.key === "Enter" || event.keyCode === 13) {
+      event.preventDefault();
+      addAmigos();
+    }
+  });
   
